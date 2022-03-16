12944006b3ab3f3b9876f6cea3ffea5cbc562cdc
OldBeliver <380506338004@yandex.ua>
2022-03-16 17:39:58 +0200

    Добавьте файлы проекта.

---
+++ LongestArray/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LongestArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            int arrayLength = 30;
+            int[] array = new int[arrayLength];
+
+            int minValue = 1;
+            int maxValue = 5;
+
+            int quantity = 0;
+            int maxQuantity = quantity;
+            string line = "";
+
+            Console.WriteLine($"Имеется одномерный массив длинной {arrayLength}, заполненный числами от {minValue} до {maxValue}");
+
+            for (int i = 0; i < arrayLength; i++)
+            {
+                array[i] = rand.Next(minValue, maxValue + 1);
+                Console.Write($"{array[i]} ");
+            }
+            Console.WriteLine();
+
+            Array.Sort(array);
+            Console.WriteLine($"массив отсортирован:");
+            foreach (int i in array)
+            {
+                Console.Write(i + " ");
+            }
+            Console.WriteLine($"\n\nсводная таблица повторений чисел:");
+            
+            for (int i = 0; i < array.Length; i++)
+            {
+                int index;
+                for (index = i; index < array.Length; index++)
+                {
+                    if (array[i] == array[index])
+                    {
+                        quantity++;
+                    }
+
+                    if (array[i] != array[index] || index == array.Length - 1)
+                    {
+                        Console.WriteLine($"значение {array[i]} - {quantity} раз");
+
+                        string newLine = $"число {array[i]} повторяется {quantity} раз";
+
+                        if (quantity == maxQuantity)
+                        {
+                            line += ",\n" + newLine;
+                        }
+
+                        if (quantity > maxQuantity)
+                        {
+                            maxQuantity = quantity;
+                            line = newLine;
+                        }
+
+                        quantity = 1;
+                        i = index;
+                    }
+                }
+            }
+            Console.WriteLine($"\nмаксимальное количество повторений:\n{line}");
+        }
+    }
+}
---
+++ LongestArray/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("LongestArray")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("LongestArray")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("ff2aec41-08bd-4dcb-9fa6-86dc7a8a71e0")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
